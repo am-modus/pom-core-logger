41d5b94092ad43cab44855f8ea51b806eb5dee32
Asim Mittal <asim.mittal@gmail.com>
2018-02-09 19:05:32 -0500

    updating package name for linter

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 
 const loggersAvailable = fs.readdirSync('loggers');
+for (let i = 0; i < loggersAvailable.length; i++) {
+    loggersAvailable[i] = loggersAvailable[i].replace('.js', '');
+}
 
 class POMLogger {
     constructor(type) {
